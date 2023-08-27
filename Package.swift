0aece9e6f0e8063d6a49ce77039d9dc28f0c0568
Alexander Baraley <alexander.baraley@lanars.com>
2023-08-27 17:19:10 +0300

    Incorporate currentDirectoryPath value to determine localizations destination path

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
   name: "LocoScript",
   platforms: [.macOS(.v12), .iOS(.v15)],
+  products: [.executable(name: "LocoScript", targets: ["LocoScript"])],
   targets: [
     .executableTarget(name: "LocoScript")
   ]
--- Sources/LocoScript/LocoScript.swift
+++ Sources/LocoScript/LocoScript.swift
@@ -1,10 +1,17 @@
+
+import Foundation
+
 @main
 enum LocoScript {
 
   static func main() async throws {
+    let currentDirectoryPath = FileManager.default.currentDirectoryPath
+
+    print("⚠️ currentDirectoryPath: \(currentDirectoryPath)")
+
     let loader = StringsLoader(locoAPIKey: "oD8Sxz1qDdVJ4DZyx1jW6C8Q_u5X9Eyy")
 
-    let destinationDirectory = "/Users/baraley/Projects/LANARS/LocoScript/Localization/"
+    let destinationDirectory = currentDirectoryPath + "/Localization/"
 
     let tags = [
       Tag(name: "common", folderPath: destinationDirectory),
